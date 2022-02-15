42972c3a2c44bbb84b60efcbf9a66972d85190d6
AlvinzYusuf <yusuuualvinz002@gmail.com>
2022-02-15 14:13:54 +0700

    commit

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,5 @@
-import styles from "../styles/Home.module.css";
+import Link from "next/link";
 
 export default function Home() {
-  return (
-    <div className={styles.container}>
-      <h1 className="text-4xl font-bold">Hello World</h1>
-    </div>
-  );
+  return <></>;
 }
