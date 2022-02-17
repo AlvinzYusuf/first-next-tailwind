a9bd943bad72c0bc851d4f15e8744941ff42fc7d
AlvinzYusuf <yusuuualvinz002@gmail.com>
2022-02-18 06:46:12 +0700

    changes configuration taillwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,6 +11,25 @@ module.exports = {
         mono: ["IBM Plex Mono", "ui-monospace"],
         poppins: ["Poppins, sans-serif"],
       },
+      colors: {
+        primarygray: {
+          DEFAULT: "#F7F7FB",
+          "200": "#E8E8FB",
+          "300": "#BFBFE3"
+        },
+        yellow: {
+          "50": "#ff100109",
+          "100": "#fffdf0",
+          "200": "#fef8be",
+          "300": "#fdf390",
+          "400": "#fded5e",
+          "500": "#fce72c",
+          "600": "#e7d103",
+          "700": "#ab9a02",
+          "800": "#6f6402",
+          "900": "#322d01"
+        }
+      }
     },
   },
   plugins: [
